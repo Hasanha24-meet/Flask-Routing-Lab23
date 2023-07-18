ba3cbe1de29e1d0092605e36ef42102aba5bb58e
Hasanhamdan789 <HasanHamdan789@gmail.com>
2023-07-18 17:49:04 +0300

    done

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, redirect, request, render_template, url_for
 
 
-app = Flask(  # Create a flask app
-    __name__,
-    template_folder='templates',  # Name of html file folder
-    static_folder='static'  # Name of directory for static files
-)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 # Your code should be below
+@app.route('/')
+def home_page():
+    return render_template("home.html")
 
+@app.route('/product')
+def product_page():
+    return render_template('product.html')
 
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
 
 
 # Your code should be above
